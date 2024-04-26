af3fa36fa89b9e41630becd7212af8b3947d121f
airforcey <masonhe608@gmail.com>
2024-04-26 11:41:04 -0400

    trts

--- Sketch.java
+++ Sketch.java
@@ -2,21 +2,44 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch extends PApplet {
-  PImage img;
+  PImage imgMushroom;
 
+// x and y coordinate for mushroom
+  float fltMushroomX = 0;
+  float fltMushroomY = 0;
 
+  // speed for the mushroom
+  float fltXSpeed = 3;
+  float fltYSpeed = 3;
   public void settings() {
-    size(800, 800);
+    size(400, 400);
   }
 
   public void setup() {
     background(210, 255, 173);
-    img = loadImage("png.png");
+    // load image
+    imgMushroom = loadImage("png.png");
+    // resize image
+    imgMushroom.resize(imgMushroom.width/4, imgMushroom.height/4);
 
   }
 
   public void draw() {
-  image(img, 0, 0);
+
+
+  // moving 
+  image(imgMushroom. fltMushroomX, fltMushroomY);
+
+    fltMushroomX += fltXSpeed;
+    fltMushroomY += fltYSpeed;
+ 
+    if (fltMushroomX > width - imgMushroom.width) {
+      fltXSpeed *= -1;
+    }
+
+    if (fltMushroomY > height - imgMushroom.height || fltMushroomY < 0) {
+      fltYSpeed *= -1;
+    }
 
   
 }
